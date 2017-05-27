e4580a588dbe75a22b66f3074f5f79c4805780c6
fongelias <fongelias@gmail.com>
2017-05-27 10:16:46 -0400

    Test kanban config changes

--- dev/app/kanban.js
+++ dev/app/kanban.js
@@ -7,6 +7,7 @@ import routes from './config/kanban.routes';
 import { board } from './components/board/board';
 import { task } from './components/task/task';
 import dashboardCtrl from './pages/dashboard/dashboardCtrl';
+import boardCtrl from './components/board/board.controller';
 
 const requires = [
 	'ngRoute',
@@ -15,6 +16,8 @@ const requires = [
 //App
 angular.module('kanban',requires)
 		.controller('dashboardCtrl', dashboardCtrl)
+		.controller('boardCtrl', boardCtrl)
 		.component('board', board)
 		.component('task', task)
 		.config(routes);
+
