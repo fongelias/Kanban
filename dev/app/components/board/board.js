c0bce8cdc914009f6763e3d4ba9ab363602a7523
fongelias <fongelias@gmail.com>
2017-05-28 08:47:51 -0400

    namespace changes

--- dev/app/components/board/board.js
+++ dev/app/components/board/board.js
@@ -1,11 +1,11 @@
 //Dependencies
 import boardTemplate from './boardTemplate.html';
-import boardCtrl from './board.controller';
 
 //Component
 export const board = {
 	template: boardTemplate,
-	controller: boardCtrl,
+	controller: 'boardCtrl',
+	controllerAs: 'boardCtrl',
 	bindings: {
 		board: '=',
 		onDelete: '&',
